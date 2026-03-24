ffc645e81a7df31da6f88dfc519fe3f378008632
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-24 11:33:56 -0400

    Mejora y correccion del sidebar usando use client

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,25 +1,71 @@
+"use client";
+
+import { MENU_ITEMS } from "@/lib/constants/navigation";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {Menu, X} from "lucide-react";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   return (
-    <aside className="w-64 bg-slate-900 text-white h-screen p-6 flex flex-col fixed">
-      <h1 className="text-xl font-bold mb-10 text-blue-400">Fundamentos Nextjs</h1>
-      <nav className="flex-1">
-        <ul className="space-y-4">
-          <li>
-            <Link href="/" className="hover:text-blue-400 transition">Inicio</Link>
-          </li>
-          <li>
-            <Link href="/ventas" className="hover:text-blue-400 transition">Ventas</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="text-xs text-slate-500">Version - v1.0.0</div>
-    </aside>
+    <>
+      {/* Boton hamburguesa */}
+      <button
+        onClick={toggleSidebar}
+        className="fixed top-4 left-4 z-50 p-2 bg-slate-800 text-white rounded-md sm:hidden"
+      >
+        {isOpen ? <X size={24}/> : <Menu size={24}/>}
+      </button>
+
+      {/* Menu lateral ASIDE */}
+      <aside className={`
+        h-screen flex flex-col
+        fixed inset-y-0 left-0 z-40 w-64 bg-slate-800 text-white p-6 transition-transform duration-300 ease-in-out
+        ${isOpen ? "translate-x-0" : "-translate-x-full"}
+        sm:translate-x-0
+      `}>
+        <div className="shrink-0">
+          <h2 className="text-xl text-center font-bold mb-5 text-blue-400">Admin Panel</h2>
+        </div>
+        <nav className="flex-1 overflow-y-auto pr-2 custom-scrollbar">
+          <ul className="space-y-2 py-2">
+            {MENU_ITEMS.map((item) => {
+              const isActive = pathname === item.href;
+
+              return (
+                <li key={item.href}>
+                  <Link 
+                    href={item.href} 
+                    onClick={() => setIsOpen(false)}
+                    className={`block p-2 rounded-lg hover:bg-white/80 hover:text-gray-800 text-lg font-semibold transition-colors ${
+                      isActive
+                      ? "bg-gray-100 text-gray-800"
+                      : "hover:bg-gray-100 hover:text-gray-800"
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              )
+              })}
+          </ul>
+        </nav>
+        <div className="text-xs text-slate-500">Version - 1.0.0</div>
+      </aside>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-30 sm:hidden"
+          onClick={toggleSidebar}
+        />
+      )}
+    </>
+
   )
 }
-
-
-
-
-
