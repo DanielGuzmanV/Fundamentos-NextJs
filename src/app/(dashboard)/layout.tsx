0e9cb7f21815a004902802e87f7f6fc92e2a0bfb
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-24 11:35:19 -0400

    Correccion del layout, nueva page inventario agregado y mejora de la page ventas

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,15 +4,11 @@ export default function DashboardLayout({
   children
 }: {children: React.ReactNode}) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <Sidebar/>
-      <main className="ml-64 flex-1 min-h-screen bg-slate-50">
+      <main className="flex-1 bg-slate-50 sm:ml-64">
         {children}
       </main>
     </div>
   )
 }
-
-
-
-
