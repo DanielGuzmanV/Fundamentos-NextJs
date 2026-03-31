b8be826e32cfe3b5db84e38c7da472d1e9022bf2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-31 12:17:40 -0400

    Se mejoro la navegacion para las rutas

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -1,5 +1,33 @@
-export const MENU_ITEMS = [
-  { name: 'Inicio', href: '/' },
-  { name: 'Ventas', href: '/ventas' },
-  { name: 'Inventario', href: '/inventario' },
-];
+import { BookOpen, LampDesk, LayoutDashboard, LucideIcon, Settings } from "lucide-react";
+
+export const PATHS = {
+  HOME: '/',
+  PROJECT_1: '/proyecto_1',
+  DEMO_1: '/demos/project-demo-1',
+  DEMO_2: '/demos/project-demo-2',
+  SETTINGS: '/settings',
+  LOGIN: '/login',
+} as const;
+
+export interface NavItemConfig {
+  name: string;
+  icon: LucideIcon;
+  href?: string;
+  isSubmenu?: boolean;
+  children?: {name: string, href: string}[];
+}
+
+export const MENU_ITEMS: NavItemConfig[] = [
+  {name: 'Dashboard', icon: LayoutDashboard, href: PATHS.HOME},
+  {name: 'Proyecto 1', icon: LampDesk, href: PATHS.PROJECT_1},
+  {
+    name: 'Demos',
+    icon: BookOpen,
+    isSubmenu: true,
+    children:[
+      {name: 'Crypto Dashboard', href: PATHS.DEMO_1},
+      {name: 'IA-Powered Chatbot', href: PATHS.DEMO_2},
+    ]
+  },
+  {name: 'Ajustes', icon: Settings, href: PATHS.SETTINGS}
+]
