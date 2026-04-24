e5168ca1d819edcb8503a94a46abde6063b9009f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-24 13:01:23 -0400

    Se creo links para navegacion del header

---
+++ src/features/demos/crypto-dashboard/main/constants/navLinks.ts
@@ -0,0 +1,15 @@
+import { PATHS } from "@/lib/constants/paths";
+import { History, LineChart, LucideIcon, PieChart, Repeat } from "lucide-react";
+
+interface PropsLinks {
+  name: string;
+  href: string;
+  icon: LucideIcon;
+}
+
+export const navLinks: PropsLinks[] = [
+  { name: "Mercado", href: PATHS.CRYPTO_DASHBOARD, icon: LineChart },
+  { name: "Portfolio", href: PATHS.CRYPTO_PORTFOLIO, icon: PieChart },
+  { name: "Swap", href: PATHS.CRYPTO_SWAP, icon: Repeat },
+  { name: "Historial", href: PATHS.CRYPTO_HISTORY, icon: History },
+]
