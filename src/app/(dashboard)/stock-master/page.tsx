3f9d885033ba9232c64f183b6cb7ad8de9030939
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-29 11:19:19 -0400

    Creacion de la ruta con la pantalla de presentacion para el primer proyecto

--- src/app/(dashboard)/stock-master/page.tsx
+++ src/app/(dashboard)/stock-master/page.tsx
@@ -1,12 +1,5 @@
+import  StockMasterHero  from "@/features/projects/stock-master/presentation/screens/StockMasterHero";
+
 export default function StockMaster() { 
-  return (
-
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
-        <h1 className="text-black font-bold text-2xl">Pagina para de Stock Master</h1>
-        
-      </div>
-    </div>
-
-  ) 
+  return <StockMasterHero/>
 }
