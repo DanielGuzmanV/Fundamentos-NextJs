d36e2b2f49bed1ab2b0577cbf80d07940b1fd47c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-29 10:32:51 -0400

    Se ajusto el nombre de la rutas del proyecto 1 y se agrego la ruta del proyecto 2

--- src/lib/constants/paths.ts
+++ src/lib/constants/paths.ts
@@ -1,6 +1,7 @@
 export const PATHS = {
   HOME: '/',
-  PROJECT_1: '/proyecto_1',
+  PROJECT_1: '/stock-master',
+  PROJECT_2: '/proyecto-2',
   DEMO_1: '/demos/demo-1',
   DEMO_2: '/demos/demo-2',
   SETTINGS: '/settings',
