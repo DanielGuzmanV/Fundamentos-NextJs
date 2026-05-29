dd9d572acc8eba48645e88f5ba0b4336831cf6d8
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-29 10:31:56 -0400

    Se cambio el nombre de la carpeta proyecto 1 por el nuevo proyecto a realizar

--- src/app/(dashboard)/stock-master/page.tsx
+++ src/app/(dashboard)/stock-master/page.tsx
@@ -1,9 +1,9 @@
-export default function ProyectoOne() { 
+export default function StockMaster() { 
   return (
 
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
-        <h1 className="text-black font-bold text-2xl">Pagina para el primer proyecto</h1>
+        <h1 className="text-black font-bold text-2xl">Pagina para de Stock Master</h1>
         
       </div>
     </div>
