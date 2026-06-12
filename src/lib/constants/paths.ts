ee65d699625040c904e3bf862940aa1b51ac6a6e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-06-12 10:54:02 -0400

    Se ajustaron los paths para la nueva estructura y se agrego el path de la nueva demo

--- src/lib/constants/paths.ts
+++ src/lib/constants/paths.ts
@@ -2,14 +2,11 @@ export const PATHS = {
   HOME: '/',
   PROJECT_1: '/stock-master',
   PROJECT_2: '/proyecto-2',
-  DEMO_1: '/demos/demo-1',
-  DEMO_2: '/demos/demo-2',
+  DEMO_1: '/quickblog',
+  DEMO_2: '/demo-2',
   SETTINGS: '/settings',
   LOGIN: '/login',
 
-  // Rutas de la demo Crypto Dashboard
-  CRYPTO_DASHBOARD: '/crypto-dashboard',
-  CRYPTO_PORTFOLIO: '/crypto-dashboard/portfolio',
-  CRYPTO_SWAP: '/crypto-dashboard/swap',
-  CRYPTO_HISTORY: '/crypto-dashboard/history'
+  // Rutas de la demo QuickBlog
+  QUICKBLOG_HOME: '/quick-blog',
 } as const;
