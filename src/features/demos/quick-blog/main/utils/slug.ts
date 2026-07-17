a0c41a746f905766e20b8ba560b82dbfeda7a748
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-17 16:49:24 -0400

    Se creo la funcion para slugiifcar un titulo

---
+++ src/features/demos/quick-blog/main/utils/slug.ts
@@ -0,0 +1,12 @@
+// Funcion para slugificar un titulo (ej. "Mi Gran Artículo" -> "mi-gran-articulo")
+export function slugify(text: string): string {
+  return text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-');
+}
