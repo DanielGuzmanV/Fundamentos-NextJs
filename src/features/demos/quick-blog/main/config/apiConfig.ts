7672b18f51172391878d53bcf1201fc60318c4aa
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-17 16:50:21 -0400

    Se configuraron las url de cada API

---
+++ src/features/demos/quick-blog/main/config/apiConfig.ts
@@ -0,0 +1,11 @@
+// URL de JSONPlaceholder
+export const JSON_PLACEHOLDER_BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+// URL de RandomUser
+export const RANDOM_USER_API_URL = 'https://randomuser.me/api/';
+
+// URL de Picsum
+export const LOREM_PICSUM_BASE_URL = 'https://picsum.photos';
+
+// Endpoint especifico
+export const RANDOM_USER_BLOG_AUTHORS_URL = `${RANDOM_USER_API_URL}?results=10`;
