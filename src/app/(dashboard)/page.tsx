d6787ffd51da4fea7bc8ff90b166429814d7b0bd
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-15 11:58:03 -0400

    Se refactorizo la ruta de la home

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,5 @@
+import HomeScreen  from "@/features/home/screens/HomeScreen";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          To get started, edit the page.tsx file.
-        </h1>
-        <p className="mt-4">El proyecto está limpio y listo.</p>
-      </div>
-    </div>
-  );
+  return <HomeScreen/>
 }
