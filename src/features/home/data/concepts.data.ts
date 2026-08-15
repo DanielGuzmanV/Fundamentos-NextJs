36cc3cd92033989b60fae6e00ccc9981ea46b883
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-15 11:54:41 -0400

    Se agrego el resto de la data en la lista

--- src/features/home/data/concepts.data.ts
+++ src/features/home/data/concepts.data.ts
@@ -1,10 +1,10 @@
 import { ConceptLevel } from "../types/concept.types";
-// import { ADVANCED_CONCEPTS } from "./concepts/advanced_concepts";
+import { ADVANCED_CONCEPTS } from "./concepts/advanced_concepts";
 import { BASIC_CONCEPTS } from "./concepts/basic_concepts";
-// import { INTERMEDIATE_CONCEPTS } from "./concepts/intermediate_concepts";
+import { INTERMEDIATE_CONCEPTS } from "./concepts/intermediate_concepts";
 
 export const CONCEPT_LEVELS_DATA: ConceptLevel[] = [
   BASIC_CONCEPTS,
-  // INTERMEDIATE_CONCEPTS,
-  // ADVANCED_CONCEPTS
+  INTERMEDIATE_CONCEPTS,
+  ADVANCED_CONCEPTS
 ]
