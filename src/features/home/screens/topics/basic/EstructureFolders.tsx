894d229bc10d440f3ef14cbf383582bbf410b571
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-19 16:32:19 -0400

    Creacion de la pantalla de prueba de topic basics

---
+++ src/features/home/screens/topics/basic/EstructureFolders.tsx
@@ -0,0 +1,9 @@
+export default function EstructureFolders() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
+        <h2 className="text-black font-bold text-2xl">Pantalla para la estructura de folders</h2>
+      </div>
+    </div>
+  ) 
+}
