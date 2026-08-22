91b86110da0639c6cff75f5c6b5a9182fc4a4567
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-22 16:59:42 -0400

    Refactorizacion del dashboard shell usando los headers nuevos

--- src/components/layout/DashboardShell.tsx
+++ src/components/layout/DashboardShell.tsx
@@ -1,21 +1,16 @@
 "use client";
 import { useState } from "react";
-import { Menu } from "lucide-react";
 import { SidebarCustom } from "@/components/layout/sidebar";
 import { useActiveTitle } from "@/hooks/useActiveTitle";
-import { PageHeader } from "./PageHeader";
+import { HeaderMobile } from "./HeaderMobile";
+import { HeaderDesktopWeb } from "./HeaderDesktopWeb";
 
 
 export function DashboardShell({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const activeTitle = useActiveTitle()
-
-  const openMobileSidebar = () => {
-    setIsSidebarOpen(true);
-    setIsCollapsed(false);
-  }
+  const {title, isSubscreen, breadcrumbs} = useActiveTitle()
 
   return (
     <div className="flex h-screen w-full bg-gray-100 overflow-hidden text-slate-900">
@@ -27,16 +22,22 @@ export function DashboardShell({ children }: { children: React.ReactNode }) {
       />
 
       <div className="flex-1 flex flex-col min-w-0 transition-all duration-200">
-        <header className="lg:hidden flex items-center justify-between p-2 dark:bg-black dark:text-zinc-50">
-          <button onClick={openMobileSidebar} className="p-2 text-gray-200">
-            <Menu size={24} />
-          </button>
-          <span className="font-bold text-lg pr-5">{activeTitle}</span>
-        </header>
-        
+        {/* Header mobile */}
+        <HeaderMobile
+          isSubscreen={isSubscreen}
+          setIsSidebarOpen={() => setIsSidebarOpen(true)}
+          title={title}
+        />
+
         {/* Contenido principal */}
         <div className='hidden lg:block sticky top-0 z-10 bg-gray-50 px-3 lg:px-6'>
-          <PageHeader title={activeTitle}/>
+          {/* Header web-desktop */}
+          <HeaderDesktopWeb
+            title={title}
+            breadcrumbs={breadcrumbs}
+            isSubscreen={isSubscreen}
+          />
+
         </div>
         <main className="flex-1 overflow-y-auto bg-gray-50">
           {children}
