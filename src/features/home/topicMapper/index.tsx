7c8e726b9c7f6a3414722adf9ecef3e130e3a6d8
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-29 23:28:14 -0400

    refactor: Se agrego la nueva ruta de un componente al topic mapper de componentes

--- src/features/home/topicMapper/index.tsx
+++ src/features/home/topicMapper/index.tsx
@@ -3,7 +3,9 @@ import dynamic from "next/dynamic";
 
 export const TOPIC_COMPONENTS_MAPPER: Record<string, ComponentType> = {
   // Basicos
-  "estructura-de-carpetas": dynamic(() => import("../screens/topics/basic/EstructureFolders"))
+  "estructura-de-carpetas": dynamic(() => import("../screens/topics/basic/EstructureFolders")),
+  "routing-dinamico": dynamic(() => import("../screens/topics/basic/RoutingDynamicScreen")),
+  
   // Intermedios
 
   // Avanzados
